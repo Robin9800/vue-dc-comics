a7c854f63694056d8df686f288c747c2e6bc3993
Robin9800 <robin.costanzo@gmail.com>
2022-04-06 16:39:37 +0200

    main footer content

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,29 @@
 <template>
   <footer>
-      <ul>
+      <div class="header-footer">
+        <ul class="info">
           <li>bla</li>
-      </ul>
+        </ul>
+      </div>
+
+      <div class="main-footer">
+          <ul>
+              <h4>DC COMICS</h4>
+              <li>peppe</li>
+          </ul>
+          <ul>
+              <h4>DC</h4>
+              <li>peppe</li>
+          </ul>
+          <ul>
+              <h4>SITES</h4>
+              <li>peppe</li>
+          </ul>
+          <ul>
+              <h4>SHOP</h4>
+              <li>peppe</li>
+          </ul>
+      </div>
   </footer>
 </template>
 
@@ -17,7 +38,21 @@ export default {
             color: white;
             background-color: hsl(209deg 98% 49%);
         }
-        footer ul li{
+    .header-footer ul li{
             margin: 40px 20px;
         }
+    .main-footer{
+        display: flex;
+        background-color: grey;
+    }.main-footer ul{
+        margin: 20px;
+        flex-direction: column;
+    }
+    .main-footer li{
+        margin: 0px
+    }
+    .info{
+        display: flex;
+        flex-direction: column;
+    }
 </style>
